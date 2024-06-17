8f2c6d7fdc6339229c3237ef2c6097d208f00060
rishitgupta203 <rishitgupta203@gmail.com>
2024-06-18 02:11:24 +0530

    Minor Error Handled

--- src/models/flight.js
+++ src/models/flight.js
@@ -11,15 +11,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Airplanes, {
+      this.belongsTo(models.Airplane, {
         foreignKey: 'airplaneId',
         as: 'airplaneDetail'
       });
-      this.belongsTo(models.Airports, {
+      this.belongsTo(models.Airport, {
         foreignKey: 'departureAirportId',
         as: 'departureAirport'
       });
-      this.belongsTo(models.Airports, {
+      this.belongsTo(models.Airport, {
         foreignKey: 'arrivalAirportId',
         as: 'arrivalAirport'
       })
